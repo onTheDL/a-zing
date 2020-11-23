b5eb93d46a305212775997b22261197ec50c383f
onTheDL <daniel.laserna@protonmail.com>
2020-11-22 22:49:40 -0500

    Add grid generation with verticals and horizontals

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const width = 600;
 const height = 600;
@@ -25,11 +19,24 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-  Bodies.rectangle(width/2, 0, width, 40, { isStatic: true }),
-  Bodies.rectangle(width/2, height, width, 40, { isStatic: true }),
-  Bodies.rectangle(0, height/2, 40, height, { isStatic: true }),
-  Bodies.rectangle(width, height/2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 World.add(world, walls);
 
+// Maze generation
+const grid = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(false));
 
+const verticals = Array(3)
+  .fill(null)
+  .map(() => Array(2).fill(false));
+
+const horizontals = Array(2)
+  .fill(null)
+  .map(() => Array(3).fill(false));
+
+console.log(horizontals);
